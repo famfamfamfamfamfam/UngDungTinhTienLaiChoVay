a5a5a0612780b82e18e6a3bdb53f50fe6d337ba5
BinhTran <fam.out16@gmail.com>
2024-08-12 20:17:13 +0700

    Add cac files cua bai thuc hanh ung dung tinh tien lai cho vay

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace THUCHANHCAULENHLAP
+{
+    class UngDungTinhTienChoVayNangLai
+    {
+        static void Main(string[] args)
+        {
+            double sotien;
+            int sothang;
+            double laisuat;
+
+            Console.WriteLine("Vui long nhap lan luot so tien gui, so thang gui va ti le lai suat");
+            sotien = double.Parse(Console.ReadLine());
+            sothang = int.Parse(Console.ReadLine());
+            laisuat = double.Parse(Console.ReadLine());
+
+            double tonglaisuat = 0;
+            for (int i = 0; i < sothang; i++)
+            {
+                tonglaisuat += sotien * (laisuat/100)/12;
+            }
+            Console.WriteLine("Lai suat la "+tonglaisuat);
+        }
+    }
+}
